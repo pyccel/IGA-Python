bdfac397fd20609e63a0680ba1279bda48ecc58f
jowezarek <julian.owezarek@tum.de>
2025-03-25 14:28:50 +0100

    minor beauty updates

--- exercises/utils.py
+++ exercises/utils.py
@@ -12,10 +12,17 @@ from psydac.linalg.block            import BlockVectorSpace
 from psydac.linalg.stencil          import StencilVectorSpace
 
 
-def get_unit_vector(v, n1, n2, pads1, pads2):
+def get_unit_vector(v, n1, n2, n3, pads1, pads2, pads3):
 
     v *= 0.0
-    v._data[pads1+n1, pads2+n2] = 1.
+    if n3 is None:
+        assert pads3 is None
+        if n2 is None:
+            raise NotImplementedError('This get_unit_vector method is only implemented in 2D.')
+        else:
+            v._data[pads1+n1, pads2+n2] = 1.
+    else:
+        raise NotImplementedError('This get_unit_vector method is only implemented in 2D.')
     
     return v
 
@@ -42,96 +49,22 @@ def toarray(A):
     for k, Wk in enumerate(codomain_blocks):
         w *= 0.
         v = Wk.zeros()
-        npts1, npts2 = Wk.npts
-        pads1, pads2 = Wk.pads
-        for n1 in range(npts1):
-            for n2 in range(npts2):
-                e_n1_n2 = get_unit_vector(v, n1, n2, pads1, pads2)
-                if W_is_block:
-                    w[k] = e_n1_n2
-                    e_n1_n2 = w
-                A_n1_n2 = At @ e_n1_n2
-                A_arr[start_index + n1*npts2+n2, :] = A_n1_n2.toarray()
+        if len(Wk.npts) == 2:
+            npts1, npts2 = Wk.npts
+            pads1, pads2 = Wk.pads
+            for n1 in range(npts1):
+                for n2 in range(npts2):
+                    e_n1_n2 = get_unit_vector(v, n1, n2, None, pads1, pads2, None)
+                    if W_is_block:
+                        w[k] = e_n1_n2
+                        e_n1_n2 = w
+                    A_n1_n2 = At @ e_n1_n2
+                    A_arr[start_index + n1*npts2+n2, :] = A_n1_n2.toarray()
+        else:
+            raise NotImplementedError('This toarray method is currently only implemented in 2D.')
         start_index += Wk.dimension
 
     return A_arr
-
-def plot(gridsize_x, title, funs, titles, gridsize_y=None, surface_plot=False):
-
-    if gridsize_y is None:
-        x = np.linspace(0, 1, gridsize_x+1)
-        vals = [[] for fun in funs]
-        for i, fun in enumerate(funs):
-            for xi in x:
-                vals[i].append(fun(xi))
-        
-        n_plots = len(funs)
-        if n_plots > 1:
-            assert n_plots == len(titles)
-        else:
-            print('Warning [plot]: will discard argument titles for a single plot')
-
-        fig = plt.figure(figsize=(2.6+4.8*n_plots, 4.8))
-        fig.suptitle(title, fontsize=14)
-
-        for i in range(n_plots):
-            ax = fig.add_subplot(1, n_plots, i+1)
-            ax.plot(x, vals[i])
-            ax.set_xlabel(r'$x$')
-            if n_plots > 1:
-                ax.set_title ( titles[i] )
-        plt.show()
-    elif gridsize_y is not None:
-        x = np.linspace(0, 1, gridsize_x+1)
-        y = np.linspace(0, 1, gridsize_y+1)
-        xx, yy = np.meshgrid(x, y)
-        vals = [[] for fun in funs]
-        for i, fun in enumerate(funs):
-            for xi, yi in zip(xx, yy):
-                vals[i].append([fun(xii, yii) for xii, yii in zip(xi, yi)])
-            vals[i] = np.array(vals[i])
-
-        n_plots = len(funs)
-        if n_plots > 1:
-            assert n_plots == len(titles)
-        else:
-            if titles:
-                print('Warning [plot]: will discard argument titles for a single plot')
-
-        fig = plt.figure(figsize=(2.6+4.8*n_plots, 4.8))
-        fig.suptitle(title, fontsize=14)
-
-        for i in range(n_plots):
-            vmin = np.min(vals[i])
-            vmax = np.max(vals[i])
-            cnorm = colors.Normalize(vmin=vmin, vmax=vmax)
-            ax = fig.add_subplot(1, n_plots, i+1)
-            ax.contourf(xx, yy, vals[i], 50, norm=cnorm, cmap='viridis')
-            ax.axis('equal')
-            fig.colorbar(cm.ScalarMappable(norm=cnorm, cmap='viridis'), ax=ax,  pad=0.05)
-            ax.set_xlabel( r'$x$', rotation='horizontal' )
-            ax.set_ylabel( r'$y$', rotation='horizontal' )
-            if n_plots > 1:
-                ax.set_title ( titles[i] )
-        plt.show()
-
-        if surface_plot:
-            fig = plt.figure(figsize=(2.6+4.8*n_plots, 4.8))
-            fig.suptitle(title+' -- surface', fontsize=14)
-
-            for i in range(n_plots):
-                vmin = np.min(vals[i])
-                vmax = np.max(vals[i])
-                cnorm = colors.Normalize(vmin=vmin, vmax=vmax)
-                ax = fig.add_subplot(1, n_plots, i+1, projection='3d')
-                ax.plot_surface(xx, yy, vals[i], norm=cnorm, cmap='viridis',
-                            linewidth=0, antialiased=False)
-                fig.colorbar(cm.ScalarMappable(norm=cnorm, cmap='viridis'), ax=ax,  pad=0.05)
-                ax.set_xlabel( r'$x$', rotation='horizontal' )
-                ax.set_ylabel( r'$y$', rotation='horizontal' )
-                if n_plots > 1:
-                    ax.set_title ( titles[i] )
-            plt.show()
 
 class H1BoundaryProjector2D(LinearOperator):
     def __init__(self, V0, V0_vs, periodic=[False, False]):
@@ -145,6 +78,8 @@ class H1BoundaryProjector2D(LinearOperator):
 
         self._BC        = self._get_BC()
         
+    def copy(self):
+        return H1BoundaryProjector2D(self._space, self._domain, self._periodic)
 
     def _get_BC(self):
 
@@ -195,3 +130,56 @@ class H1BoundaryProjector2D(LinearOperator):
         v.copy(out=out)
         apply_essential_bc(out, *BC)
         return out
+
+def plot(gridsize_x, gridsize_y, title, funs, titles, surface_plot=False):
+
+    x = np.linspace(0, 1, gridsize_x+1)
+    y = np.linspace(0, 1, gridsize_y+1)
+    xx, yy = np.meshgrid(x, y)
+    vals = [[] for _ in funs]
+    for i, fun in enumerate(funs):
+        for xi, yi in zip(xx, yy):
+            vals[i].append([fun(xii, yii) for xii, yii in zip(xi, yi)])
+        vals[i] = np.array(vals[i])
+
+    n_plots = len(funs)
+    if n_plots > 1:
+        assert n_plots == len(titles)
+    else:
+        if titles is not None:
+            print('Warning [plot]: will discard argument titles for a single plot')
+
+    fig = plt.figure(figsize=(2.6+4.8*n_plots, 4.8))
+    fig.suptitle(title, fontsize=14)
+
+    for i in range(n_plots):
+        vmin = np.min(vals[i])
+        vmax = np.max(vals[i])
+        cnorm = colors.Normalize(vmin=vmin, vmax=vmax)
+        ax = fig.add_subplot(1, n_plots, i+1)
+        ax.contourf(xx, yy, vals[i], 50, norm=cnorm, cmap='viridis')
+        ax.axis('equal')
+        fig.colorbar(cm.ScalarMappable(norm=cnorm, cmap='viridis'), ax=ax,  pad=0.05)
+        ax.set_xlabel( r'$x$', rotation='horizontal' )
+        ax.set_ylabel( r'$y$', rotation='horizontal' )
+        if n_plots > 1:
+            ax.set_title ( titles[i] )
+    plt.show()
+
+    if surface_plot:
+        fig = plt.figure(figsize=(2.6+4.8*n_plots, 4.8))
+        fig.suptitle(title+' -- surface', fontsize=14)
+
+        for i in range(n_plots):
+            vmin = np.min(vals[i])
+            vmax = np.max(vals[i])
+            cnorm = colors.Normalize(vmin=vmin, vmax=vmax)
+            ax = fig.add_subplot(1, n_plots, i+1, projection='3d')
+            ax.plot_surface(xx, yy, vals[i], norm=cnorm, cmap='viridis',
+                        linewidth=0, antialiased=False)
+            fig.colorbar(cm.ScalarMappable(norm=cnorm, cmap='viridis'), ax=ax,  pad=0.05)
+            ax.set_xlabel( r'$x$', rotation='horizontal' )
+            ax.set_ylabel( r'$y$', rotation='horizontal' )
+            if n_plots > 1:
+                ax.set_title ( titles[i] )
+        plt.show()
